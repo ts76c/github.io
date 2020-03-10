27464ff559d56bec0c2d9c4b119fd935a52de29e
ts76cn <55599388+ts76cn@users.noreply.github.com>
2020-03-10 23:17:56 +0900

    Create ini.php

---
+++ ini.php
@@ -0,0 +1,25 @@
+<?php
+
+//以下は、環境に応じて変更をお願い致します。
+//$API_key = 'xxxxxxxxxxxx';
+
+//----------------------------------------------------
+// データベース関連
+//----------------------------------------------------
+// データベースの種類
+define("_DB_TYPE", "mysql");
+// データベースホスト名
+define("_DB_HOST", "localhost");
+// データベース接続ユーザー名
+define("_DB_USER", "map_user");
+// データベース接続パスワード
+define("_DB_PASS", "xxxxxxxxxxxx");
+// データベース名
+define("_DB_NAME", "map");
+// テーブル名
+define("_TABLE_NAME", "geo_locations");
+
+// データソースネーム
+//define("_DSN", _DB_TYPE . ":host=" . _DB_HOST . ";dbname=" . _DB_NAME. ";charset=utf8");
+
+?>
